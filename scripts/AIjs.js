b98a9b8308ad8192b6fd1117e5b30599bfea84d0
arcanisonline <60981821+arcanisonline@users.noreply.github.com>
2020-03-02 18:57:47 +0100

    Update AIjs.js

--- scripts/AIjs.js
+++ scripts/AIjs.js
@@ -23,18 +23,3 @@ async function loadModel(){
     model = await tf.loadLayersModel("scripts/model.json");
     console.log(model.summary());
 }
-                
-                
- function loadJSON(callback) {   
-
-    var xobj = new XMLHttpRequest();
-        xobj.overrideMimeType("application/json");
-    xobj.open('GET', '../scripts/model.json', true); // Replace 'my_data' with the path to your file
-    xobj.onreadystatechange = function () {
-          if (xobj.readyState == 4 && xobj.status == "200") {
-            // Required use of an anonymous callback as .open will NOT return a value but simply returns undefined in asynchronous mode
-            callback(xobj.responseText);
-          }
-    };
-    xobj.send(null);  
- }
